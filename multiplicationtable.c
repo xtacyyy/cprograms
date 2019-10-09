9829268b725dcac438a4fbdcbdc9f3aa3dc5cd5e
Zeeko <50589846+xtacyyy@users.noreply.github.com>
2019-10-10 00:39:05 +0400

    Update multiplicationtable.c

--- multiplicationtable.c
+++ multiplicationtable.c
@@ -2,11 +2,12 @@
 #include<conio.h>
 void main()
 {
+    //*Varialbes*//
     int i, n;
-
+    //*User input*//
     printf("Enter a number for its multiplication table: ");
     scanf("%d", &n);
-
+    
     for(i=1; i<=20; i++)
     {
         printf("%d x %d = %d\n", n,i,n*i);
